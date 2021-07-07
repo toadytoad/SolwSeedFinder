a4723949f7fdbad052fae795c5290a37bc68321e
Toadytop <tom.philip.ca@gmail.com>
2021-07-06 23:51:21 -0400

    Final version of SOLW seed finder. Everything seems to work well. Any suggestions would be greatly appreciated in discord. Csv file will get several extra commits later on, feel free to use is for filtering solw seeds further.

--- src/main/java/SOLW_V7/Main.java
+++ src/main/java/SOLW_V7/Main.java
@@ -4,7 +4,7 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.ThreadLocalRandom;
 public class Main {
-    static long StructureSeed = 0;
+    static long StructureSeed = 194736563634066L;
     static ThreadPoolExecutor pool;
     static Object lock = new Object();
 
--- src/main/java/SOLW_V7/SeedFinder.java
+++ src/main/java/SOLW_V7/SeedFinder.java
@@ -15,10 +15,11 @@ import kaptainwutax.mcutils.util.pos.BPos;
 import kaptainwutax.mcutils.util.pos.CPos;
 import kaptainwutax.mcutils.version.MCVersion;
 import kaptainwutax.terrainutils.TerrainGenerator;
-
-import java.util.List;
-
 import static kaptainwutax.featureutils.loot.item.Items.*;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
 
 public class SeedFinder implements Runnable{
     long structureSeed;
@@ -56,11 +57,12 @@ public class SeedFinder implements Runnable{
         };
         PillagerOutpost pillagerOutpost = new PillagerOutpost(MCVersion.v1_17); //wutax outpost object
         BuriedTreasure buriedTreasure = new BuriedTreasure(MCVersion.v1_17); //wutax buried treasure object
+
         int buriedTreasureIndex = 0; //indices for arrays
         int pillagerOutpostIndex = 0;
         int closeStructuresIndex = 0;
 
-        System.out.print(","); //notifies that the program has started a new structure seed
+        //System.out.print(","); //notifies that the program has started a new structure seed
         for (int x = -16; x < 16; x++) {
             for (int z = -16; z < 16; z++) { //initial iterators for buried treasures
                 CPos pos = buriedTreasure.getInRegion(structureSeed, x, z, cr); //gets the buriedTreasure in the region
@@ -116,7 +118,7 @@ public class SeedFinder implements Runnable{
                 if (dist<48) { //checks for distance between structures
                     nearStructs[closeStructuresIndex] = new NearStructs(pillagerOutposts[j], buriedTreasures[i], dist); //appends a new class to nearStructs
                     closeStructuresIndex++;
-                    System.out.println(structureSeed+" "+pillagerOutposts[j]+" "+buriedTreasures[i]+"!");
+                    System.out.println("!");
                 }
             }
         }
@@ -126,34 +128,48 @@ public class SeedFinder implements Runnable{
                 nearStructs[i]=null;
             }
         }
-        SeedIterator seedIterator = WorldSeed.getSisterSeeds(structureSeed);
-        while (seedIterator.hasNext()) {
-            long worldSeed = seedIterator.next();
-            int tempBiome;
-            OverworldBiomeSource biomeSource = new OverworldBiomeSource(MCVersion.v1_17, worldSeed);
-            boolean foundBiome = false;
-            int index = 0;
-            while(nearStructs[index]!=null){
-                if (pillagerOutpost.canSpawn(nearStructs[index].getOutpost().toChunkPos(), biomeSource)&&buriedTreasure.canSpawn(nearStructs[index].getTreasure(), biomeSource)){
-                    int treasureX = nearStructs[index].getTreasure().getX();
-                    int treasureZ = nearStructs[index].getTreasure().getZ();
-                    for (int x = treasureX-48; x<treasureX+49; x+=16){
-                        for(int z = treasureZ-48; z<treasureZ+49; z+=16){
-                            tempBiome = biomeSource.getBiome(x,0,z).getId();
-                            if (tempBiome==44||tempBiome==47){
-                                foundBiome=true;
-                                System.out.println("Found seed: " + worldSeed);
-                            }
-                            if (foundBiome){
-                                break;
+        FileWriter csv;
+        if (nearStructs[0]!=null) {
+            try {
+                csv = new FileWriter("C:\\Users\\__toad_\\IdeaProjects\\SolwSeedFinder\\src\\main\\java\\SOLW_V7\\seeds.csv", true);
+                SeedIterator seedIterator = WorldSeed.getSisterSeeds(structureSeed);
+                while (seedIterator.hasNext()) {
+                    long worldSeed = seedIterator.next();
+                    int tempBiome;
+                    OverworldBiomeSource biomeSource = new OverworldBiomeSource(MCVersion.v1_17, worldSeed);
+                    boolean foundBiome = false;
+                    int index = 0;
+                    while (nearStructs[index] != null) {
+                        if (pillagerOutpost.canSpawn(nearStructs[index].getOutpost().toChunkPos(), biomeSource) && buriedTreasure.canSpawn(nearStructs[index].getTreasure(), biomeSource)) {
+                            int treasureX = nearStructs[index].getTreasure().toBlockPos().getX()+9;
+                            int treasureZ = nearStructs[index].getTreasure().toBlockPos().getZ()+9;
+                            for (int x = treasureX - 48; x < treasureX + 49; x += 16) {
+                                for (int z = treasureZ - 48; z < treasureZ + 49; z += 16) {
+                                    tempBiome = biomeSource.getBiome(x, 0, z).getId();
+                                    if (tempBiome == 44 || tempBiome == 47) {
+                                        foundBiome = true;
+
+                                        csv.append(worldSeed + "," + nearStructs[index].getOutpost().getX() + "," + nearStructs[index].getOutpost().getZ() + "," + treasureX + "," + treasureZ + "\n");
+                                        System.out.println("Found seed: " + worldSeed);
+                                    }
+                                    if (foundBiome) {
+                                        break;
+                                    }
+                                }
+                                if (foundBiome) {
+                                    break;
+                                }
                             }
                         }
-                        if (foundBiome){
-                            break;
-                        }
+                        index++;
                     }
                 }
-                index++;
+
+                System.out.println("reached flushing");
+                csv.flush();
+                csv.close();
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
         Main.StartNext();
@@ -164,7 +180,7 @@ public class SeedFinder implements Runnable{
         BuriedTreasureGenerator generator=new BuriedTreasureGenerator(mcVersion);
         generator.generate(null, pos, new ChunkRand());
         List<ChestContent> chestContents=buriedTreasure.getLoot(structureSeed, generator, new ChunkRand(), false);
-        if (chestContents.get(0).containsAtLeast(TNT, 4)){
+        if (chestContents.get(0).containsAtLeast(TNT, 8)){
             if(chestContents.get(0).containsAtLeast(IRON_INGOT, 2)){
                 return chestContents.get(0).containsAtLeast(IRON_INGOT, 4) || chestContents.get(0).containsAtLeast(GOLD_INGOT, 2);
             }
